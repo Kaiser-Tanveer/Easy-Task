eb1e2d3e65320346fa78c96f1c11ccce0fa39ff0
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-12-30 01:56:25 +0600

    active route implemented

--- src/Pages/Home/Blog/Blog.js
+++ src/Pages/Home/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Blog = () => {
     return (
-        <div className='my-5'>
+        <Container className='my-5'>
             <h2>Coming soon..</h2>
-        </div>
+        </Container>
     );
 };
 
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthContext/AuthProvider';
 const Header = () => {
     const { user, logOut } = useContext(AuthContext);
@@ -16,26 +16,38 @@ const Header = () => {
             })
     }
     return (
-        <Navbar collapseOnSelect expand="lg" className='bg-info pb-lg-0'>
+        <Navbar collapseOnSelect expand="lg" className='bg-info pb-lg-0 shadow' style={{ zIndex: '1000' }}>
             <Container>
-                <Link to='/' className='text-decoration-none text-light fw-bold fs-2'><span className='text-warning'>E</span>asy<span className='text-warning'>T</span>ask</Link>
+                <Link to='/' className='text-decoration-none text-light fw-bold fs-1'><span className='text-warning'>E</span>asy<span className='text-warning'>T</span>ask</Link>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav fill variant='tabs' className='flex mx-auto mb-0 pb-0'>
-                        <Nav.Link eventKey="link-1"><Link to='/' className='link-hover text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow'>Add Task</Link></Nav.Link>
-                        <Nav.Link eventKey="link-2"><Link to='/myTask' className='link-hover text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow'>My Task</Link></Nav.Link>
-                        <Nav.Link eventKey="link-3"><Link to='/completed' className='link-hover text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow'>Completed Task</Link></Nav.Link>
-                        <Nav.Link eventKey="link-4"><Link to='/blog' className='link-hover text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow'>Blog</Link></Nav.Link>
+                    <Nav fill variant='tabs' className='flex mx-auto'>
+                        <Nav.Link><NavLink to='/' className={
+                            ({ isActive }) => isActive ? 'isActive shadow text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow bg-white' :
+                                'text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow'
+                        }>Add Task</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to='/myTask' className={
+                            ({ isActive }) => isActive ? 'isActive shadow text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow bg-white' :
+                                'text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow'
+                        }>My Task</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to='/completed' className={
+                            ({ isActive }) => isActive ? 'isActive shadow text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow bg-white' :
+                                'text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow'
+                        }>Completed Task</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to='/blog' className={
+                            ({ isActive }) => isActive ? 'isActive shadow text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow bg-white' :
+                                'text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 shadow'
+                        }>Blog</NavLink></Nav.Link>
                         {
                             user && user?.uid ?
-                                <Nav.Link onClick={logOutHandler} eventKey="link-5"><Link className='link-hover text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 bg-danger border shadow'>Logout</Link></Nav.Link>
+                                <Nav.Link onClick={logOutHandler}><NavLink className='text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 bg-danger border shadow'>Logout</NavLink></Nav.Link>
                                 :
-                                <Nav.Link eventKey="link-5"><Link to='/register' className='link-hover text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 bg-warning border shadow'>Register</Link></Nav.Link>
+                                <Nav.Link><NavLink to='/register' className='text-dark text-decoration-none px-4 rounded-top text-decoration-none fs-5 p-2 my-sm-2 w-100 bg-warning border shadow'>Register</NavLink></Nav.Link>
                         }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </Navbar >
     );
 };
 
